2c3131b088d7a125a4a15f672417b33741199a6a
Nayara <nayaralima@poli.ufrj.br>
2023-03-11 11:50:04 -0300

    feat: delete and update comments

--- node/app/Controllers/Http/CommentsController.ts
+++ node/app/Controllers/Http/CommentsController.ts
@@ -21,4 +21,32 @@ export default class CommentsController {
       data: comment,
     }
   }
+
+  public async destroy({ params }: HttpContextContract) {
+    const comment = await Comment.findOrFail(params.id)
+
+    await comment.delete()
+
+    return {
+      message: 'Comentário excluído com sucesso!',
+      data: comment,
+    }
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const body = request.body()
+    const id = params.id
+
+    const comment = await Comment.findOrFail(id)
+
+    comment.username = body.username
+    comment.text = body.text
+
+    await comment.save()
+
+    return {
+      message: 'Comentário atualizado com sucesso!',
+      data: comment,
+    }
+  }
 }
